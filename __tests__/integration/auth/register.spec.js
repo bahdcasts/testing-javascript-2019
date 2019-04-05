b028c8f9a3755fb977e36778aba6b78d7d074732
Kati Frantz <bahdcoder@gmail.com>
2019-04-05 12:28:36 +0100

    lesson-2-testing-failed-validations

--- __tests__/integration/auth/register.spec.js
+++ __tests__/integration/auth/register.spec.js
@@ -9,20 +9,38 @@ import { disconnect } from '@tests/utils/mongoose'
 const app = () => supertest(server)
 
 describe('The register process', () => {
+    const REGISTER_ENDPOINT = '/api/v1/auth/register'
+    let user = {
+        name: 'Test User',
+        email: 'test@user.com',
+        password: 'password'
+    }
+
     beforeEach(async () => {
         await User.deleteMany({})
     })
 
     it('should register a new user', async () => {
-        const response = await app().post('/api/v1/auth/register').send({
-            name: 'Test User',
-            email: 'test@user.com',
-            password: 'password'
-        })
+        const response = await app().post(REGISTER_ENDPOINT).send(user)
 
         expect(response.status).toBe(200)
         expect(response.body.data.token).toBeDefined()
         expect(response.body.message).toBe('Account registered.')
+    })
+
+    it('should return a 422 if registration fails', async () => {
+        // prepare
+        await User.create(user)
+
+        // action
+        const response = await app().post(REGISTER_ENDPOINT).send(user)
+
+        // assertion
+        expect(response.status).toBe(422)
+        expect(response.body.message).toBe('Validation failed.')
+        expect(response.body.data.errors).toEqual({
+            email: 'This email has already been taken.'
+        })
     })
 
     afterAll(async () => {
