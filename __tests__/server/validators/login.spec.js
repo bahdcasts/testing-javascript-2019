723ffa1fab36ce54389aef5ebedab2e18a99378c
Kati Frantz <bahdcoder@gmail.com>
2019-04-05 10:36:52 +0100

    lesson-7-mocks-and-spies

--- __tests__/server/validators/login.spec.js
+++ __tests__/server/validators/login.spec.js
@@ -3,6 +3,18 @@
  */
 import loginValidator from '@validators/login'
 
+class Response {
+    status(status) {
+        this.status = status
+
+        return this
+    }
+
+    json(data) {
+        return data
+    }
+}
+
 describe('The login validator', () => {
     it('should call the next function when validation succeeds', async () => {
         const req = {
@@ -18,5 +30,29 @@ describe('The login validator', () => {
         await loginValidator(req, res, next)
 
         expect(next).toHaveBeenCalled()
+    })
+
+    it('should return a 422 if validation fails', async () => {
+        const req = {
+            body: {
+                password: 'password'
+            }
+        }
+        const next = jest.fn()
+        const res = new Response()
+
+        const statusSpy = jest.spyOn(res, 'status')
+        const jsonSpy = jest.spyOn(res, 'json')
+
+        await loginValidator(req, res, next)
+        expect(statusSpy).toHaveBeenCalledWith(422)
+        expect(jsonSpy).toHaveBeenCalledWith({
+            message: 'Validation failed.',
+            data: {
+                errors: {
+                    'email': 'email is a required field'
+                }
+            }
+        })
     })
 })
