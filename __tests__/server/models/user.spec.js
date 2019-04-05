ce2e1f104f47952d527e76a435243279f5cfe316
Kati Frantz <bahdcoder@gmail.com>
2019-04-05 09:57:14 +0100

    lesson-5-testing-instance-methods

--- __tests__/server/models/user.spec.js
+++ __tests__/server/models/user.spec.js
@@ -4,33 +4,38 @@
 import User from '@models/User'
 import mongoose from 'mongoose'
 import Bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import config from '@config'
 
 describe('The User model', () => {
+    const user = {
+        name: 'Test User',
+        email: 'test@user.com',
+        password: 'password'
+    }
+    let createdUser
+
     beforeAll(async () => {
         await mongoose.connect('mongodb://localhost:27017/auth-app_test', { useNewUrlParser: true })
+        createdUser = await User.create(user)
     })
 
     it('should hash the user password before saving to the database', async () => {
-        const user = {
-            name: 'Test User',
-            email: 'test@user.com',
-            password: 'password'
-        }
-
-        const createdUser = await User.create(user)
         expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
     })
 
     it('should set the email confirm code for the user before saving to the database', async () => {
-        const user = {
-            name: 'Test User',
-            email: 'test@user.com',
-            password: 'password'
-        }
-
-        const createdUser = await User.create(user)
-
         expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+    })
+
+    describe('The generateToken method', () => {
+        it('should generate a valid jwt for a user', () => {
+            const token = createdUser.generateToken()
+
+            const { id } = jwt.verify(token, config.jwtSecret)
+
+            expect(id).toEqual(JSON.parse(JSON.stringify(createdUser._id)))
+        })
     })
 
     afterAll(async () => {
