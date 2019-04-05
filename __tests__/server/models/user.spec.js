23e1933e9603f8e920f87471d8806bc3da3b3995
Kati Frantz <bahdcoder@gmail.com>
2019-04-05 09:21:36 +0100

    lesson-4-setup-and-teardown

--- __tests__/server/models/user.spec.js
+++ __tests__/server/models/user.spec.js
@@ -6,9 +6,11 @@ import mongoose from 'mongoose'
 import Bcrypt from 'bcryptjs'
 
 describe('The User model', () => {
-    it('should hash the user password before saving to the database', async () => {
+    beforeAll(async () => {
         await mongoose.connect('mongodb://localhost:27017/auth-app_test', { useNewUrlParser: true })
+    })
 
+    it('should hash the user password before saving to the database', async () => {
         const user = {
             name: 'Test User',
             email: 'test@user.com',
@@ -17,7 +19,21 @@ describe('The User model', () => {
 
         const createdUser = await User.create(user)
         expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
+    })
 
+    it('should set the email confirm code for the user before saving to the database', async () => {
+        const user = {
+            name: 'Test User',
+            email: 'test@user.com',
+            password: 'password'
+        }
+
+        const createdUser = await User.create(user)
+
+        expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+    })
+
+    afterAll(async () => {
         await mongoose.connection.close()
     })
 })
